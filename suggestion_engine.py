868e951b67b3865cedac262a3835e15e8c11be88
MadhuVedula <madhughnavedula@gmail.com>
2025-06-05 16:21:57 +0530

    Fix import for get_suggestions

--- suggestion_engine.py
+++ suggestion_engine.py
@@ -1,6 +1,18 @@
 import re
 from tech_extractor import extract_tech_entities
 
+
+def get_suggestions(resume_text):
+    tech_data = extract_tech_entities(resume_text)
+    
+    tech_terms = set(tech_data["ner_entities"])
+    for category in tech_data["keyword_entities"]:
+        tech_terms.update(tech_data["keyword_entities"][category])
+    tech_terms = set(term.lower() for term in tech_terms)
+
+    suggestions = []
+    words = re.findall(r'\b\w+\b', resume_text)
+    
 COMMON_MISSPELLINGS = {
     "teh": "the",
     "recieve": "receive",
@@ -15,17 +27,6 @@ COMMON_MISSPELLINGS = {
     "dependecy": "dependency",
     "intermidiate": "intermediate"
 }
-
-def get_suggestions(resume_text):
-    tech_data = extract_tech_entities(resume_text)
-    
-    tech_terms = set(tech_data["ner_entities"])
-    for category in tech_data["keyword_entities"]:
-        tech_terms.update(tech_data["keyword_entities"][category])
-    tech_terms = set(term.lower() for term in tech_terms)
-
-    suggestions = []
-    words = re.findall(r'\b\w+\b', resume_text)
 
     for word in words:
         word_lower = word.lower()
